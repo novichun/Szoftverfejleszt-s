629be9e88b72bfb23e8d1b074e9492ce42c3cd5f
Novák Dániel <novakdanci@gmail.com>
2023-02-25 20:36:19 +0100

    Műveletek, dependencies, Solve

---
+++ elso-hazi/ProjectA/src/main/java/muveletek/muveletek.java
@@ -0,0 +1,22 @@
+package muveletek;
+
+public class muveletek {
+
+    public static int osszead(int a, int b) {
+        return a + b;
+    }
+
+    public static int kivon(int a, int b) {
+        return a - b;
+    }
+
+    public static double oszt(double a, double b) {
+        return a / b;
+    }
+
+    public static int szoroz(int a, int b) {
+        return a * b;
+    }
+
+
+}
--- elso-hazi/ProjectB/src/main/java/org/example/Main.java
+++ elso-hazi/ProjectB/src/main/java/org/example/Main.java
@@ -1,7 +1,17 @@
 package org.example;
-
+import static muveletek.muveletek.*;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Adjon meg két egész számot!");
+
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        System.out.println(osszead (a,b));
+        System.out.println(kivon (a,b));
+        System.out.println(szoroz (a,b));
+        System.out.println(oszt (a,b));
     }
 }
